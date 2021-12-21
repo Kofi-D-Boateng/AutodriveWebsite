35b18d05e6f1a8b689af82e7ee201e9793f3f91c
Swank123 <kdboat2@gmail.com>
2021-12-21 17:47:44 -0600

    heroku test

--- public/routes/contact.js
+++ public/routes/contact.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 require("dotenv").config();
 // MAILGUN TEST
-const formData = require("form-data");
-const Mailgun = require("mailgun.js");
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({ username: "api", key: process.env.MAILGUN_API });
+// const formData = require("form-data");
+// const Mailgun = require("mailgun.js");
+// const mailgun = new Mailgun(formData);
+// const mg = mailgun.client({ username: "api", key: process.env.MAILGUN_API });
 
 router.get("/", (req, res) => {
   let navbarLoggedIn = "partials/loggedIn-navbar.ejs";
@@ -21,31 +21,6 @@ router.get("/", (req, res) => {
       navbar: navbar,
       teamEmail: process.env.TEAM_EMAIL,
     });
-  }
-});
-
-router.post("/team-help", (req, res) => {
-  if (req.isAuthenticated()) {
-    try {
-      var data = {
-        from: "Kofi Boateng <kofiboateng47@gmail.com>",
-        to: process.env.TEAM_EMAIL,
-        subject: "AUTOMATED TESTING",
-        text: "If you see this then it worked",
-      };
-
-      mg.messages.create(
-        process.env.MAILGUN_DOMAIN,
-        data,
-        function (error, body) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(data);
-          }
-        }
-      );
-    } catch (error) {}
   }
 });
 
